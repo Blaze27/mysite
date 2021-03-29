5d95260af247d163e62b5e3e1b00738feebc4586
Rahul Sharma <bbblaze27@gmail.com>
2021-03-29 18:16:24 +0530

    Pending fix : Secret Key

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,14 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 
-with open('secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+sec = os.environ['SECRET_KEY']
+SECRET_KEY = sec
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
