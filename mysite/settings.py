8a81973ff08c85de5ff310c85cf29e86ab8f99ba
Rahul Sharma <bbblaze27@gmail.com>
2021-03-29 17:22:34 +0530

    fix secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,11 +19,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cy9i74wt+pd34r24ptpq*=y=rf7=d-lah-gl6=bj(m2(x#qn#6'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
